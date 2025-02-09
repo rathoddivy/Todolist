1dc24e7e87e3aefb8fbbe774708a9b41cb35b272
Rathod Divyaraj <166823718+rathoddivy@users.noreply.github.com>
2025-02-09 10:24:34 +0530

    Add files via upload

---
+++ src/utils/ConstantComponent.jsx
@@ -0,0 +1,14 @@
+export const baseurl = 'https://crud-appbackend.onrender.com/api';
+
+
+import React from 'react'
+
+const ConstantComponent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ConstantComponent
